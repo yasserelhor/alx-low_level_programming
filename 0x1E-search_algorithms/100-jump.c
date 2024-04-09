4caa25ae0b2ec33a96818350275a5796753d4a94
yasserelhor <elhoryasser6@gmail.com>
2024-04-09 14:15:38 +0000

    adding the thing

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,25 +10,25 @@
  * Return: Index of the value if found, otherwise -1
  */
 
-int leap_search(int *array, size_t size, int value)
+int jump_search(int *array, size_t size, int value)
 {
-	size_t i, leap, stride;
+	size_t i, stepa, jump;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	stride = sqrt(size);
-	for (i = leap = 0; leap < size && array[leap] < value;)
+	stepa = sqrt(size);
+	for (i = jump = 0; jump < size && array[jump] < value;)
 	{
-		printf("Value checked array[%ld] = [%d]\n", leap, array[leap]);
-		i = leap;
-		leap += stride;
+		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
+		i = jump;
+		jump += stepa;
 	}
 
-	printf("Value found between indexes [%ld] and [%ld]\n", i, leap);
+	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
 
-	leap = leap < size - 1 ? leap : size - 1;
-	for (; i < leap && array[i] < value; i++)
+	jump = jump < size - 1 ? jump : size - 1;
+	for (; i < jump && array[i] < value; i++)
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
